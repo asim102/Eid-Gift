f9d509f529a74d8d9e05d61ebac05fb8682bbe52
Muhammad Asim <50958005+asim102@users.noreply.github.com>
2020-05-25 11:36:10 +0500

    Update asim.py

--- asim.py
+++ asim.py
@@ -295,7 +295,7 @@ def pilih_super():
 	print "\n\033[1;94m        ❈     \x1b[1;91mTo Stop Process Press CTRL+Z \033[1;94m    ❈"
 	print "   \033[1;92m●══════════════════◄►══════════════════●"
 
-	jalan('           \033[1;91mAahil start cloning Wait...')
+	jalan('           \033[1;91mAsim start cloning Wait...')
 	print  "  \033[1;92m ●══════════════════◄►══════════════════●" 
 
 	def main(arg):
